604696dba26a18d48e25db19d4459e27d9d5cd2b
Mohit Khandelwal <mohitkh7@gmail.com>
2016-08-03 12:20:30 +0530

    Create 16largest.py

---
+++ Assignment1/16largest.py
@@ -0,0 +1,11 @@
+#16Largest of 3 numbers
+def largest(a,b,c):
+      #return max(a,b,c)      using built in max function
+      if a>b and a>c:
+            return a
+      elif b>a and b>c:
+            return b
+      else:
+            return c
+
+      
