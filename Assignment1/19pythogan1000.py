fd1d97fc9649ef60850ff7e1d621a4d758e29140
Mohit Khandelwal <mohitkh7@gmail.com>
2016-08-04 12:44:19 +0530

    Create 19pythogan1000.py

---
+++ Assignment1/19pythogan1000.py
@@ -0,0 +1,12 @@
+#WAP to find pythogan triplet whose sum is a+b+=8
+#this is also project euler problem 9
+#mathblog.dk
+
+s=1000
+for a in range(1,s/3):
+  for b in range(1,s/2):
+    c=s-(a+b)
+    if c**2==a**2+b**2:
+      print(a,b,c)
+    
+  
